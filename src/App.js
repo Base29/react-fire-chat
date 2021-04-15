545b4ebfd2263c54671ac56a865bb115c50c4380
Faisal Hussain <faisal@base29.com>
2021-04-15 22:56:32 +0500

    initialzed firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+    apiKey: 'AIzaSyC48gOY8jK7rQaadGZAf57yGShPDZFSIl8',
+    authDomain: 'react-fire-chat-65688.firebaseapp.com',
+    projectId: 'react-fire-chat-65688',
+    storageBucket: 'react-fire-chat-65688.appspot.com',
+    messagingSenderId: '3955691014',
+    appId: '1:3955691014:web:f3fdba89ab329dde6fbd87',
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header className='App-header'></header>
+        </div>
+    );
 }
 
 export default App;
